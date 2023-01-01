f15135a3cceb7154ae5fedeaf38cdd00faf2e982
Lungman <lewisgbell@live.co.uk>
2023-01-01 20:25:14 +0000

    modified:   experiments.html 	modified:   tests/mathy.js

--- tests/mathy.js
+++ tests/mathy.js
@@ -3,13 +3,11 @@ function runTheSums (input1, input2) {
 }
 function reynolds (speed, dia) {
     let density = 1000;
-    let dynamicVisc = 0.001005; //water @ 10C
-    console.log(speed, dia);
+    let dynamicVisc = 0.001299537; //water @ 10C
     return (density*speed*dia/(dynamicVisc));
 }
 function haaland (absoluteRoughness, speed, dia){
-    let dia_m = dia/1000;
-    let reynoldNum = reynolds(speed, dia_m);
+    let reynoldNum = reynolds(speed, dia/1000);
     console.log(reynoldNum);
-    return ((-1.8*Math.log((absoluteRoughness/(3.7*dia_m)^1.11)+(6.9/reynoldNum)))**-2);
+    return ((-1.8*Math.log((absoluteRoughness/(3.7*dia)**1.11)+(6.9/reynoldNum)))**-2);
 }
