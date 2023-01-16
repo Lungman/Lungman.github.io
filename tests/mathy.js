8b1f26537ae146735ea0c92c6db1e06ce03788b5
Lungman <74768218+Lungman@users.noreply.github.com>
2023-01-16 09:28:54 +0000

    Update mathy.js

--- tests/mathy.js
+++ tests/mathy.js
@@ -6,7 +6,7 @@ function reynolds (speed, dia) {
     let dynamicVisc = 0.001299537; //water @ 10C
     return (density*speed*dia/(dynamicVisc));
 }
-function haaland (absoluteRoughness, speed, dia){
+function haaland (absoluteRoughness, speed, dia) {
     let reynoldNum = reynolds(speed, dia/1000);
     if (reynoldNum > 2500) {
     return ((-1.8*Math.log10((absoluteRoughness/(3.7*dia)**1.11)+(6.9/reynoldNum)))**-2);
@@ -16,4 +16,7 @@ function haaland (absoluteRoughness, speed, dia){
 }
 function flowToSpeed (litreRate, dia) {
     return ((litreRate/1000)/(Math.PI*(dia/2)**2));
+}
+function volCalc (dia, length) {
+    return (Math.PI*(dia/2)**2)*length;
 }
