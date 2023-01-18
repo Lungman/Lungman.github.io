ed239fa6662d06907a1dde58fd02b0fda6c608b8
Lungman <74768218+Lungman@users.noreply.github.com>
2023-01-18 12:25:15 +0000

    Update mathy.js

--- tests/mathy.js
+++ tests/mathy.js
@@ -14,6 +14,13 @@ function haaland (absoluteRoughness, speed, dia) {
         return 64/reynoldNum;
     }
 }
+function haaland2 (foo){
+    if (foo.Re<2500){
+        return 64/foo.Re;
+    } else {
+        return ((-1.8*Math.log10((foo.RelRough/3.7)**1.11)+(6.9/foo.Re)))**-2);
+    }
+}
 function flowToSpeed (litreRate, dia) {
     return ((litreRate/1000)/(Math.PI*(dia/2)**2));
 }
